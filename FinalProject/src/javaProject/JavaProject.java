fe1430dbfe85c8e95679ccd3e6f010e8fe549a7b
AkashdeepSingh-470 <Kamboj@Kamboj>
2020-12-05 01:15:49 +0530

    Done first change

---
+++ FinalProject/src/javaProject/JavaProject.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package javaProject;
+
+/**
+ * @author Kamboj
+ *
+ */
+public class JavaProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//AkashdeepSingh-470
+		System.out.println("done");
+
+	}
+
+}
