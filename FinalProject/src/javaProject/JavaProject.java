4cb50f41af87939a93bfb6df31d94e99753fd3fd
AkashdeepSingh-470 <Kamboj@Kamboj>
2020-12-12 00:56:05 +0530

    Made two methods and added some more basic functionality

--- FinalProject/src/javaProject/JavaProject.java
+++ FinalProject/src/javaProject/JavaProject.java
@@ -4,13 +4,11 @@
 package javaProject;
 
 import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
+
 /**
- * @author Kamboj
+ * @author AkashdeepSingh-470
  *
  */
 public class JavaProject {
@@ -20,69 +18,13 @@ public class JavaProject {
 	   * and open the template in the editor.
 	   */
 
-	      public static void main(String[] args) 
+	      public static void main(String[] args) throws IOException 
 	      {
-	    	  
-	          try
-	          {
-	              User U=new User();
-
-	              Update Up=new Update();
-
-	              String userName = "sir911";
+	    	  	String userName = "sir911";
 	  	  		String password = "bob";
 	  	  		methodOne(userName,password);
-	                            
-	              JOptionPane.showMessageDialog(null,"     Welcome to Simulation of a Banking System 	 ");
-	              {
-	                  String s = JOptionPane.showInputDialog(null,"Please Enter your choice"
-	                		+"\n1.Create New Account"
-		      				+"\n2.Existing Account"
-		    				+"\n3.Delete Account"
-		    				+"\n4.Exit",JOptionPane.QUESTION_MESSAGE);
-	                  int choice=Integer.parseInt(s);
-	                  
-	                  if(choice>5 || choice<1)
-	                  {
-	                	  JOptionPane.showMessageDialog(null,
-	                  		    "You Have Entered Wrong Choice"
-	                  			+"",
-	                  		    "Incorrect Choice", JOptionPane.ERROR_MESSAGE);
-	                  	JOptionPane.showConfirmDialog(null,
-	                  		    "Do you want to continue?",
-	                  		    "Confirmation",
-	                  		    JOptionPane.YES_NO_CANCEL_OPTION);
-	                      
-	                  }
-/*
-	              switch(choice)
-	              {
-	                  case 1:
-	                      U.NewUser();
-	                      U.ExistingUser();
-	                      break;
-	                  case 2:
-	                      U.ExistingUser();
-	                      break;
-	                  case 3:
-	                      U.DeleteAccount();
-	                      break;
-	                  case 4:
-	                	  JOptionPane.showMessageDialog(null,"You Have Entered Wrong Choice");
-	                      break;
-	              }*/
-	              }
-	          }
-	              
-	          catch(InputMismatchException a)
-	          {
-	        	  JOptionPane.showMessageDialog(null,"\\nOops !!! You Have Entered Wrong Input...");
-	          }
-	          catch(Exception e)
-	          {
-	        	  }
-	          }
-	  
+	  	  		
+	      }
 
 public static void methodOne(String userName, String password) throws IOException 
 {
@@ -92,8 +34,8 @@ String inputDialog2;
 int attempts = 0; //Initializing this variable
 
 //Here while loop gives only three attempts for user to login
- while(attempts < 3)
-   {
+while(attempts < 3)
+ {
 	 //taking input from user through Dialog box
 	 inputDialog = JOptionPane.showInputDialog("Enter your Username");
 	 inputDialog2 = JOptionPane.showInputDialog("Enter your Password");
@@ -103,17 +45,75 @@ int attempts = 0; //Initializing this variable
 	{
 	//displaying login success message to user
 	JOptionPane.showMessageDialog(null, "Login Success");
+	methodTwo(null);
 	break;
 	}
-   else
-   {
+ else
+ {
 	   //message for user if wrong userName and password is entered
 	   JOptionPane.showMessageDialog(null, "Invalid Username or Password Try Again!");
-	   attempts++;
+	   attempts++;  
 	   
-   }
-	
-   }
-
+ }
+ }
 	  }
+public static void methodTwo(String[] args) throws IOException 
+{
+	User U=new User();
+
+	Update Up=new Update();
+
+
+            
+	JOptionPane.showMessageDialog(null,"     Welcome to Simulation of a Banking System 	 ");
+{
+	
+  String s = JOptionPane.showInputDialog(null,"Please Enter your choice"
+		  	+"\n1.Create New Account"
+			+"\n2.Existing Account"
+			+"\n3.Delete Account"
+			+"\n4.Exit",JOptionPane.QUESTION_MESSAGE);
+  int choice=Integer.parseInt(s);
+
+switch(choice)
+{
+  case 1:
+	  /*
+      U.NewUser();
+      U.ExistingUser();*/
+      break;
+  case 2:
+	  /*
+      U.ExistingUser();*/
+      break;
+  case 3:
+   /*
+      U.DeleteAccount();*/
+      break;
+  case 4:
+	  JOptionPane.showMessageDialog(null,"Thank You for visting Simulation of a Banking System");
+      break;
+  default:
+  
+  int result = JOptionPane.showConfirmDialog(null,"You Have Entered Wrong Choice"+"\nDo you want to continue?","Confirmation",JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+  if(result == JOptionPane.YES_OPTION){
+  	String userName = "sir911";
+  	String password = "bob";
+  	methodOne(userName,password);  
+   }
+  else if (result == JOptionPane.NO_OPTION)
+  {
+      System.exit(0);
+   }
+   else 
+   {
+  	 System.exit(0);
+   }
+  	String userName = "sir911";
+	String password = "bob";
+	methodOne(userName,password);
+  break;
+}
+}
+}
 }
