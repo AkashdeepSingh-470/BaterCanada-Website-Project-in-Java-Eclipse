d9c4b5c254414bd369440602f23a6e185f60b806
AkashdeepSingh-470 <Kamboj@Kamboj>
2020-12-12 01:16:48 +0530

    now we need to make our further functions

---
+++ FinalProject/src/javaProject/functions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaProject;
+
+/**
+ * @author Kamboj
+ *
+ */
+public class functions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
